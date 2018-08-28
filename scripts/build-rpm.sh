d6b086772c8c7053863cd7f16cd790ab60e4c586
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-08-28 14:33:09 -0700

    Upgrade Site Audit to version 8.x-2.2.

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -29,7 +29,7 @@ arch='noarch'
 vendor='Pantheon'
 description='drush extensions: Pantheon rpm containing additional Drush commands'
 
-drupal_8_site_audit_version=8.x-2.1
+drupal_8_site_audit_version=8.x-2.2
 drupal_8_registry_rebuild_version=7.x-2.5
 drupal_7_site_audit_version=7.x-1.16
 drupal_7_registry_rebuild_version=7.x-2.5
